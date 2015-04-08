20e6e550d81e179e1120a6895184fb1470eb3126
Mike Chacon <mike@sicdigital.com>
2015-04-07 22:44:09 -0400

    Picking up on this project several years later

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,18 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     minifycss = require('gulp-minify-css'),
-    watch = require('gulp-watch'),
     rename = require('gulp-rename');
 
-var path = './scss/*.scss';
+var scss_path = './scss/*.scss';
 
 
 gulp.task('watch', function() {
-    //gulp.watch('js/*.js', ['lint', 'scripts']);
-    gulp.watch(path, ['sass']);
+    gulp.watch(scss_path, ['sass']);
 });
 
 
 gulp.task('sass', function() {
-    return gulp.src(path)
+    return gulp.src(scss_path)
         .pipe(sass())
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
         .pipe(gulp.dest('css'))
